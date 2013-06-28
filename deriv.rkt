2eb8364aed38c027cf236c4111da965609ef3c8a
Ryan Plessner <rpless@ccs.neu.edu>
2013-06-27 22:28:48 -0400

    added the a match-expander for constants, the sum rule and the constant division rule.

--- deriv.rkt
+++ deriv.rkt
@@ -16,18 +16,57 @@
 ;; Main
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (deriv expr var) expr)
+(define (deriv expr var)
+  (match expr 
+    [(matches-constant? var) 0]
+    [(== var) 1]
+    [(list (? plus-or-minus-sign? op) a b)
+     (list op (deriv a var) (deriv b var))]
+    [(cons '* (list-no-order (matches-constant? var a) b))
+     (list '* a (deriv b var))]
+    [else (error "Unrecognized expression: " expr)]))
+
+;; Helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (plus-or-minus-sign? op)
+  (or (eq? op '-) (eq? op '+)))
+
+;; Match Expanders
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; An Expression that matches a constant is either a number or a symbol that 
+;; is not eq? to the given symbol.
+(define-match-expander matches-constant?
+  (λ (stx)
+    (syntax-case stx ()
+      [(_ var)
+       #'(or (? number?)
+             (and (? symbol?) (== var (negate eq?))))]
+      [(_ var a) 
+       #'(or (? number? a)
+             (and (? symbol? a) (== var (negate eq?))))])))
 
 ;; Tests
 
 (module+ test 
   (define tderiv (curryr deriv 'x))
   
+  ;; derivative of a number
+  (check-equal? (tderiv 5) 0)
+  
+  ;; derivative of a constant symbol
+  (check-equal? (tderiv 'a) 0)
   
   ;; constant division Rule
   (check-equal? (tderiv '(* 5 x)) '(* 5 1))
+  (check-equal? (tderiv '(* x 5)) '(* 5 1))
+  (check-equal? (tderiv '(* x a)) '(* a 1))
   
   ;; Sum Rule
   (check-equal? (tderiv '(+ x x)) `(+ ,(tderiv 'x) ,(tderiv 'x)))
-  (check-equal? (tderiv '(- x x)) `(- ,(tderiv 'x) ,(tderiv 'x))))
+  (check-equal? (tderiv '(- x x)) `(- ,(tderiv 'x) ,(tderiv 'x)))
+  
+  ;; Check the error case for the sake of coverage
+  (check-exn exn:fail? (thunk (tderiv (list)))))
       
